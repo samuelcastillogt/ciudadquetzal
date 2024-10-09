296d6ec72c7013da9f0ca285084b8b032173cbd6
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-10-09 17:23:23 -0600

    analitycs setup

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -15,6 +15,16 @@ export default function Document() {
             `,
           }}
         />
+                <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-0SQNFDN2BF');          
+            `,
+          }}
+        />
       </Head>
       <body className="antialiased">
       <noscript
