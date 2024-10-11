085cdbd0b694e5b94ecbb16b298a43839226111e
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-10-11 16:32:19 -0600

    integracion de SSR en blog(correcccion)

--- src/pages/blog/[post].tsx
+++ src/pages/blog/[post].tsx
@@ -10,15 +10,12 @@ function HtmlRender({ htmlString }:any) {
 }
 export async function getServerSideProps(datal:any){
   const data = await apiService.getPosts()
-  console.log(data)
   return { props: { data }}
 }
 function Post({data}:any) {
   const route = useParams()
   const [post, setPost] = useState<any>(undefined)
   const [desc, setDesc] = useState<string>("Bienvenidos al Blog sobre Ciudad Quetzal.")
-  console.log(data)
-
   useEffect(() => {
     if(data.length > 0){
       const result = data.find((i:any)=> i.data.id == route.post)
@@ -27,7 +24,7 @@ function Post({data}:any) {
         const match = result.data.content.match(/<p>(.*?)<\/p>/i)
         const descripcion = match[1].replace(/<\/?[^>]+(>|$)/g, "")
         setDesc(descripcion)
-        setPost(result.data)
+        setPost(result)
       }
 
     }
@@ -35,11 +32,11 @@ function Post({data}:any) {
   return (
     <>
     <Head>
-      <title>{post && post.title}</title>
+      <title>{post && post.data.title}</title>
       <meta name="description" content={post && desc}/>
-      <link rel='image_src' href={post.img}/>
+      <link rel='image_src' href={post && post.img}/>
     </Head>
-    <div className='p-5 flex justify-center items-center post-body'>{post && <HtmlRender htmlString={post.content} />}</div>
+    <div className='p-5 flex justify-center items-center post-body'>{post && <HtmlRender htmlString={post.data.content} />}</div>
     </>
     
   )
