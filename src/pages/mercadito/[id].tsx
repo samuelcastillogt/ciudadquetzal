2cf5c8737359414bf188deafe74e2cec6bbcf1e2
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-10-14 17:02:36 -0600

    correccion de estilo en pagina de negocios

--- src/pages/mercadito/[id].tsx
+++ src/pages/mercadito/[id].tsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { useRouter } from 'next/router'
-import { useStore } from '@/store'
 import { apiService } from '@/services/api.service'
 import Head from 'next/head'
 export async function getServerSideProps(datal:any){
@@ -16,9 +14,17 @@ function BusinesPage({data}: any) {
         <meta name="keywords" content={data.descripcion} />
         <link rel='image_src' href={data.imagen}/>
     </Head>
-    <div>
-        <img src={data.imagen} />
-        <p>{data.nombre}</p>
+    <div className='flex justify-around items-center p-5 flex-wrap h-full'>
+        <img src={data.imagen} className='w-1/2'/>
+        <div className='w-1/2 p-5'>
+          <p className='text-2xl font-bold'>{data.nombre}</p>
+          <p>{data.descripcion}</p>
+          <p>Categoria: {data.categoria}</p>
+          <p>{data.direccion}</p>
+          {/* <iframe src="https://www.google.com/maps/embed?pb=!1m13!1m8!1m3!1d11719.130395997185!2d-90.57552308064162!3d14.697871601010174!3m2!1i1024!2i768!4f13.1!3m2!1m1!2s!5e0!3m2!1ses!2sgt!4v1728946484304!5m2!1ses!2sgt" style={{border:0, width: "100%"}}  loading="lazy"></iframe>
+          <a href={`https://www.google.com/maps/@${data.lat},${data.lng}`}>Ver en mapa</a> */}
+        </div>
+        
     </div>
     </>
     
