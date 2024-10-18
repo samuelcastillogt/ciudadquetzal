d7c6b3d6db21a604507c63e8712d8010ebed44e5
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-10-18 13:24:34 -0600

    integracion de sitemap dinamico

---
+++ src/pages/sitemap.xml.js
@@ -0,0 +1,43 @@
+import {apiService} from "../services/api.service.ts"
+export async function getServerSideProps({ res }) {
+    // Llamada a tu API para obtener las URLs dinámicas
+    const apiResponse = await apiService.getPosts()
+    const urls = apiResponse
+  
+    // Generar el contenido XML para el sitemap
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+    <loc>${`https://ciudadquetzal.com`}</loc>
+    <lastmod>18/10/2014</lastmod>
+    <changefreq>${'monthly'}</changefreq>
+    <priority>${'1.0'}</priority>
+  </url>
+      ${urls
+        .map((url) => {
+          return `
+            <url>
+              <loc>${`https://ciudadquetzal.com/blog/${url.data.id}`}</loc>
+              <lastmod>${url.data.updated}</lastmod>
+              <changefreq>${'monthly'}</changefreq>
+              <priority>${'1.0'}</priority>
+            </url>
+          `;
+        })
+        .join('')}
+    </urlset>`;
+  
+    // Configurar el contenido para que sea XML
+    res.setHeader('Content-Type', 'application/xml');
+    res.write(sitemap);
+    res.end();
+  
+    return {
+      props: {}, // No necesitamos pasar props para este componente
+    };
+  }
+  
+  // Este archivo no renderiza ningún componente, simplemente devuelve el XML
+  export default function Sitemap() {
+    return null;
+  }
