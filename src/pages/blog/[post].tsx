0fa076ed7f2aac2d8ceadcd33525077647f3ff2d
Samuel Castillo <samuelcastillo@MacBook-Pro-de-Samuel.local>
2024-10-29 12:37:39 -0600

    Integracion de shared button

--- src/pages/blog/[post].tsx
+++ src/pages/blog/[post].tsx
@@ -1,3 +1,4 @@
+import BlogCard from '@/components/BlogCard';
 import { apiService } from '@/services/api.service';
 import { useStore } from '@/store'
 import Head from 'next/head';
@@ -11,16 +12,17 @@ function HtmlRender({ htmlString }:any) {
 export async function getServerSideProps(datal:any){
   const data = await apiService.getPosts()
   const result = data.find((i:any)=> i.data.id == datal.query.post)
+  const rels = data.filter((i:any)=> i.data.labels[0] == result.data.labels[0]) 
   const match = result.data.content.match(/<p>(.*?)<\/p>/i)
   const descripcion = match != undefined ? match[1].replace(/<\/?[^>]+(>|$)/g, "") : "Conoce un poco mas sobre Ciudad Quetzal en nuestra web."
-  const response = {desc: descripcion, post: result}
+  const response = {desc: descripcion, post: result, rels}
 
   return { props: { data: response }}
 }
 function Post({data}:any) {
   const route = useParams()
-  const {desc, post} = data
-  console.log(data)
+  const {desc, post, rels} = data
+console.log(post)
   return (
     <>
     <Head>
@@ -28,7 +30,17 @@ function Post({data}:any) {
       <meta name="description" content={post && desc}/>
       <link rel='image_src' href={post.img}/>
     </Head>
-    <div className='p-5 flex justify-center items-center post-body w-3/4 mx-auto'>{post && <HtmlRender htmlString={post.data.content} />}</div>
+    
+    <div className='p-5 flex flex-col justify-center items-center post-body w-3/4 mx-auto border-b-4'>
+      {post && <HtmlRender htmlString={post.data.content} />}
+      <div className="fb-share-button" data-href={"https://www.ciudadquetzal.com/blog/" + post.data.id} data-layout="" data-size=""><a target="_blank" href={"https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fwww.ciudadquetzal.com%2Fblog%2F" + post.data.id} className="fb-xfbml-parse-ignore"><img src="https://www.gsrefriaire.com/wp-content/uploads/2017/09/comparte_face.png" className='w-32'/></a></div>
+      </div>
+
+      <h2 className='text-center'>Post Relacionados</h2>
+
+    <div className='flex flex-wrap p-5 justify-center items-center post-body w-3/4 mx-auto'>
+      {rels.map((i:any)=> <BlogCard data={i} key={i.img}/>)}
+    </div>
     </>
     
   )
